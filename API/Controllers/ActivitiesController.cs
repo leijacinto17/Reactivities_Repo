b284aa142c19bd1a40218c064e79b26884e54eaf
MARK LEO JACINTO <markleo.jacinto@dxc.com>
2023-02-17 16:57:57 +0800

    initial commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+﻿using API.Common;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Activity>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+    }
+}
